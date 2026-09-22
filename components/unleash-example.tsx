bcea215b13927497069bca6f4071d2e852a86481
Nuno Góis <github@nunogois.com>
2026-09-22 12:14:19 +0100

    chore: test with 2.0.0-beta.2

--- components/unleash-example.tsx
+++ components/unleash-example.tsx
@@ -1,12 +1,30 @@
-import { useFlag } from '@unleash/unleash-react-native-sdk'
+import { useFlag, useVariant } from '@unleash/unleash-react-native-sdk'
+import { View } from 'react-native'
 import { ThemedText } from './themed-text'
 
 export const UnleashExample = () => {
   const enabled = useFlag('unleash-react-native-demo')
+  const variant = useVariant('unleash-react-native-demo')
+
+  console.log(variant)
 
   return (
-    <ThemedText>
-      {enabled ? 'Feature is enabled!' : 'Feature is disabled!'}
-    </ThemedText>
+    <View style={{ gap: 4, marginBottom: 20 }}>
+      <ThemedText>
+        Feature:{' '}
+        <ThemedText
+          type='defaultSemiBold'
+          style={{ color: enabled ? '#22c55e' : '#ef4444' }}
+        >
+          {enabled ? 'Enabled' : 'Disabled'}
+        </ThemedText>
+      </ThemedText>
+      {variant.enabled && (
+        <ThemedText>
+          Variant:{' '}
+          <ThemedText type='defaultSemiBold'>{variant.name}</ThemedText>
+        </ThemedText>
+      )}
+    </View>
   )
 }
